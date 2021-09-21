efc0dead751c2ee8a6770029464221dbd41d419b
Jonah Hoffman <jh97@iu.edu>
2021-09-21 18:31:55 -0400

    commit

---
+++ app/public/js/index.js
@@ -0,0 +1,35 @@
+const ppl = {
+    data() {
+      return {
+        "person": {},
+        
+        }
+    },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date)
+            .format('D MMM YYYY');
+        }
+    },
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.person = parsedJson.results[0]
+                console.log("C");
+            })
+            .catch( err => {
+                console.error(err)
+            })
+
+            console.log("B");
+        }
+    },
+    created() {
+        this.fetchUserData();
+    }
+  }
+  
+Vue.createApp(ppl).mount('#personApp');
