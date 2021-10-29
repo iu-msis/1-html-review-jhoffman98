4d413f60f0f20d242acc9255fed619250cc5bc73
Jonah Hoffman <jh97@iu.edu>
2021-10-29 15:12:24 -0400

    kk

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -2,7 +2,9 @@ const bookApp = {
     data() {
       return {
         books: [],
+        selectedBook: null,
         bookForm:{}
+        
         }
     },
     computed: {
@@ -27,6 +29,13 @@ const bookApp = {
 
             console.log("B");
         },
+        postBook(evt) {
+            if (this.selectedBook === null) {
+                this.postNewBook(evt);
+            } else {
+                this.postEditBook(evt);
+            }
+          },
         postNewBook(evt) { //event handler for form submission, event object is the default 
             //select the student id and add another offer into this student     
             console.log("Posting:", this.bookForm);
@@ -48,7 +57,62 @@ const bookApp = {
                 // reset the form
                 this.bookForm = {};
             });
-        }
+        },
+        
+        postEditBook(evt) {
+            this.bookForm.id = this.selectedBook.id;       
+            
+            console.log("Updating!", this.bookForm);
+      
+            fetch('api/books/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                this.resetBookForm();
+              });
+          },
+        
+          postDeleteBook(o) {
+            if (!confirm("Are you sure you want to delete the book")) {
+                return;
+            }
+            
+            fetch('api/books/delete.php', {
+                method:'POST',
+                body: JSON.stringify(o),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                this.resetBookForm();
+              });
+          },
+
+            selectBook(o) {
+            this.selectedBook = o;
+            this.bookForm = Object.assign({}, this.selectedBook);
+            },
+            resetBookForm() {
+                this.selectedBook = null;
+                this.bookForm = {};
+            }
+
+
     },
     created() {
         this.fetchBookData();
